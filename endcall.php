102cdd9df88462db8f0f5bb6a9c1d0b4507a2d63
Wit <wannama.pawit@gmail.com>
2018-01-07 20:37:07 +0700

    Display

--- endcall.php
+++ endcall.php
@@ -154,7 +154,7 @@ if (isset($_GET['date']) && !empty($_GET['date'])) {
                                         </div>
                                     </div>
                                     <div id="agentCalc" class="<?= (isset($_GET['report']) && $_GET['report'] == "sum") ? "show" : "" ?>" >
-                                      Play Agent 
+                                      Display Agent 
                                         <div>
                                             <label style="margin-left: 25px;"> <input type="radio" name="calc" value="indata" checked> With Score </label> 
                                             <div>
