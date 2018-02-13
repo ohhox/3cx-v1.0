e66cdad163fbdfc96620a51330b821136c20517d
Wit <wannama.pawit@gmail.com>
2018-02-13 21:28:12 +0700

    End Call

--- _endcall_excel_2.php
+++ _endcall_excel_2.php
@@ -53,6 +53,14 @@ $objPHPExcel->setActiveSheetIndex(2)
 
 $i = 2;
 $data = array();
+$NullValue = array();
+$Page3Data = array(
+    'success' => 0,
+    'dontSuccess' => 0,
+    'score' => array(
+        "NULL" => 0, 0 => 0, 1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0
+    )
+);
 foreach ($list AS $key => $value) {
     $score = "";
     if (empty($value['score'])) {
@@ -60,10 +68,18 @@ foreach ($list AS $key => $value) {
             $score = $value['score'];
         } else {
             $score = "NULL";
+            $NullValue[$i] = $value;
         }
+        $Page3Data['dontSuccess'] ++;
     } else {
         $score = $value['score'];
+        $Page3Data['success'] ++;
+        $Page3Data['score'][$score] ++;
     }
+
+
+
+
     $objPHPExcel->setActiveSheetIndex(2)
             ->setCellValue("A$i", ($i - 1))
             ->setCellValue("B$i", $fn->redate($value['DateLeave'], 'no'))
@@ -103,7 +119,8 @@ $objPHPExcel->setActiveSheetIndex(2)->getStyle("A1:K$i")->getAlignment()->setHor
 /* * ************* END PAGE 3 ************ */
 
 /* * ************* PAGE 2 ************ */
- 
+
+
 if (($_GET['Project']) == 2) {
     $objPHPExcel->setActiveSheetIndex(1)
             ->setCellValue('A1', 'Name Agent')
@@ -173,7 +190,7 @@ if (($_GET['Project']) == 2) {
             ->setCellValue('J2', 'ไม่พึงพอใจ')
             ->setCellValue('K2', '%')
             ->setCellValue('L2', '% ความพึงพอใจ');
-     $x = 3;
+    $x = 3;
     foreach ($data as $key => $value) {
         $all = $value['score'][1] + $value['score'][2] + $value['score'][3] + $value['score']['NULL'];
         $objPHPExcel->setActiveSheetIndex(1)
@@ -181,14 +198,116 @@ if (($_GET['Project']) == 2) {
                 ->setCellValue("B$x", $value['agent'])
                 ->setCellValue("D$x", $value['count'])
                 ->setCellValue("F$x", $value['score']['NULL'])
-                ->setCellValue("G$x", $value['score'][1]+ $value['score'][0])
+                ->setCellValue("G$x", $value['score'][1] + $value['score'][0])
                 ->setCellValue("H$x", $value['score'][1])
                 ->setCellValue("J$x", $value['score'][0]);
         $x++;
     }
 }
- 
+
 /* * ************* END PAGE 2 ************ */
+
+/* * ************* PAGE 1 ************ */
+$objPHPExcel->setActiveSheetIndex(0)
+        ->setCellValue('A1', ' รายงานความพึงพอใจลูกค้าด้านบริการ Pruksa Contact Center 1739 ')
+        ->setCellValue('A2', 'จำนวนสายที่ Contact Center ให้บริการทั้งหมด (ACD+Transection)')
+        ->setCellValue('B2', 'จำนวนสาย ที่พนักงานส่งเข้าประเมิน')
+        ->setCellValue('C2', 'สาเหตุที่ไม่ส่งสายประเมิน')
+        ->setCellValue('C3', 'ลูกค้าไม่สะดวกประเมิน')
+        ->setCellValue('D3', 'ลูกค้าวางสายไปทันทีเมื่อได้รับข้อมูลเรียบร้อยแล้ว')
+        ->setCellValue('E3', 'พนักงานพฤกษาติดต่อเข้ามา / โอนสายระหว่างศูนย์')
+        ->setCellValue('F3', 'ลูกค้า Eng')
+        ->setCellValue('G3', 'สายหลุด / สายเงียบ /โทรผิด โรคจิต')
+        ->setCellValue('H2', '% การส่งสายเข้าประเมิน')
+        ->setCellValue('I2', '% ที่ไม่ส่งสายเข้าประเมิน')
+        ->setCellValue('J2', 'จำนวนสายที่ประเมินผลได้สำเร็จ')
+        ->setCellValue('K2', 'จำนวนสายที่วางไปก่อนประเมินแล้วเสร็จ')
+        ->setCellValue('B4', $Page3Data['success'] + $Page3Data['dontSuccess'])
+        ->setCellValue('J4', $Page3Data['success'])
+        ->setCellValue('K4', $Page3Data['dontSuccess']);
+
+
+$objPHPExcel->setActiveSheetIndex(0)
+        ->mergeCells('A2:A3')
+        ->mergeCells('B2:B3')
+        ->mergeCells('C2:G2')
+        ->mergeCells('H2:H3')
+        ->mergeCells('I2:I3')
+        ->mergeCells('J2:J3')
+        ->mergeCells('K2:K3');
+
+
+
+$objPHPExcel->setActiveSheetIndex(0)
+        ->setCellValue('A6', ' คะแนนความพึงพอใจลูกค้าต่อการบริการ Pruksa Contact Center 1739 ')
+        ->setCellValue('A7', 'จำนวนสายที่ประเมินผลได้สำเร็จ')
+        ->setCellValue('B7', 'ภาพรวมการให้บริการทุกประเภท');
+if (($_GET['Project']) == 2) {
+
+    $objPHPExcel->setActiveSheetIndex(0)
+            ->setCellValue('C2', 'สาเหตุที่ไม่ส่งสายประเมิน')
+            ->setCellValue('B8', 'Press 1')
+            ->setCellValue('C8', 'Press 2')
+            ->setCellValue('D8', 'Press 3')
+            ->setCellValue('E8', '% ความพึงพอใจ');
+
+    $objPHPExcel->setActiveSheetIndex(0)
+            ->mergeCells('A7:A8')
+            ->mergeCells('B7:E7');
+
+    $objPHPExcel->setActiveSheetIndex(0)
+            ->setCellValue('B9', $Page3Data['score'][1])
+            ->setCellValue('C9', $Page3Data['score'][2])
+            ->setCellValue('D9', $Page3Data['score'][3])
+            ->setCellValue('E9', '');
+} else if (($_GET['Project']) == 3) {
+
+    $objPHPExcel->setActiveSheetIndex(0)->setCellValue('C2', 'สาเหตุที่ไม่ส่งสายประเมิน')
+            ->setCellValue('B8', 'พอใจ')
+            ->setCellValue('C8', 'ไม่พอใจ')
+            ->setCellValue('D8', '% ความพึงพอใจ');
+
+    $objPHPExcel->setActiveSheetIndex(0)
+            ->mergeCells('A7:A8')
+            ->mergeCells('B7:D7');
+
+
+    $objPHPExcel->setActiveSheetIndex(0)
+            ->setCellValue('B9', $Page3Data['score'][1])
+            ->setCellValue('C9', $Page3Data['score'][0])
+            ->setCellValue('D9', '');
+}
+
+
+
+
+$objPHPExcel->setActiveSheetIndex(0)
+        ->setCellValue('A11', 'รายละเอียดสายที่ลูกค้าประเมิน "ไม่พึงพอใจ"')
+        ->setCellValue('A12', 'No.')
+        ->setCellValue('B12', 'Date')
+        ->setCellValue('C12', 'Time')
+        ->setCellValue('D12', 'Tel No.')
+        ->setCellValue('E12', 'Agent name');
+$i = 13;
+ 
+foreach ($NullValue AS $k => $v) {
+
+    $objPHPExcel->setActiveSheetIndex(0)
+            ->setCellValue("A$i", $i - 13)
+            ->setCellValue("B$i", $fn->redate($v['DateLeave']))
+            ->setCellValue("C$i", $fn->retime($v['time']))
+            ->setCellValue("D$i", $v['customernumber'])
+            ->setCellValue("E$i", $v['name']);
+    $i++;
+}
+
+
+
+
+
+/* * ************* END PAGE 1 ************ */
+
+
 // Redirect output to a clientâ€™s web browser (Excel2007)
 header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
 header('Content-Disposition: attachment;filename="EndCallSurveyReports.xlsx"');
